3652e824ada0e0875b4f6470f103d0e48ac68da5
Hubert Schipkowski <schipkowski@sincerelycontent.com>
2020-12-02 14:44:50 +0100

    201202e

--- Converter.swift
+++ Converter.swift
@@ -5,20 +5,26 @@
 // Version: 0.1
 // 02.12.20
 
-func run() {
-    prepareData()
+func greet() {
+    print("Hi, this is a little binary to decimal converter.")
+    run()
 }
 
-func prepareData() {
+func run() {
     informUser()
     getData()
-    proofData(data: getData())
-    filterData()
-    separateParts()
+    prepareData()
+    operate()
+    askForMoreAction()
 }
 
 func informUser() {
-    print("Hi, this is a little binary to decimal converter.\nYou can input the binary number (e.G. 1010.1101) here:")
+    print("You can input the binary number (e.G. 1010.1101) here:")
+}
+
+func prepareData() {
+    proofData(data: getData())
+    filterData()
 }
 
 func getData() -> String {
@@ -29,17 +35,32 @@ func proofData(data: String) {}
 
 func filterData() {}
 
-func separateParts() {
+func operate() {
     operateBeforeComma()
     operateBehindComma()
+    giveData()
 }
 
 func operateBeforeComma() {}
 func operateBehindComma() {}
+
 func giveData() {}
 
+func askForMoreAction() {
+    print("Do you want to convert another binary?")
+    let possibleAnswers = ["y","n"]
+    let answer = possibleAnswers.randomElement() ?? "y"
+    if answer == "y" {
+        run()
+    } else {
+        close()
+    }
+}
+
+func close() {}
 
 
-// Action
 
-run()
+// Execute:
+
+greet()
