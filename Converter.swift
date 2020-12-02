d7032776fa1fe57c21e9073d834cb6310857d72f
Hubert Schipkowski <schipkowski@sincerelycontent.com>
2020-12-02 13:51:18 +0100

    201202a

---
+++ Converter.swift
@@ -0,0 +1,45 @@
+
+// A tiny Binary to Decimal Converter:
+
+// (c) Hubert Schipkowski
+// Version: 0.1
+// 02.12.20
+
+func run() {
+    prepareData()
+}
+
+func prepareData() {
+    informUser()
+    getData()
+    proofData(data: getData())
+    filterData()
+    separateParts()
+}
+
+func informUser() {
+    print("Hi, this is a little binary to decimal converter.\nYou can input the binary number (e.G. 1010.1101) here:")
+}
+
+func getData() -> String {
+    return ""
+}
+
+func proofData(data: String) {}
+
+func filterData() {}
+
+func separateParts() {
+    operateBeforeComma()
+    operateBehindComma()
+}
+
+func operateBeforeComma() {}
+func operateBehindComma() {}
+func giveData() {}
+
+
+
+// Action
+
+run()
